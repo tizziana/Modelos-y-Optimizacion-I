3de8b6e036cd89b9b60741b577d4e8b44229b446
tizziana <tizzianamazza@gmail.com>
2022-08-24 02:59:55 -0300

    Implementacion de la solucion

--- entrega_PrimerProblema.py
+++ entrega_PrimerProblema.py
@@ -10,23 +10,48 @@ def mannage_line(lavanderia, line):
 		lavanderia.agregar_prenda(line[1], line[2])
 
 
-
-def main():
-	lavanderia = Lavanderia()
+def creacion_grafo():
 	try:
+		lavanderia = Lavanderia()
 		with open("Primer_Problema.txt", 'r') as file:
 			
 			for line in file:
-				mannage_line(lavanderia, line.split())			
+				mannage_line(lavanderia, line.split())		
+
+		return lavanderia	
 
 	except Exception as e:
 		print("[ERROR] No se pudo abrir el archivo de lavanderia.")
-		#print(repr(e))
-	
-	#print(lavanderia.obtener_cant_prendas())
-	#print(lavanderia.obtener_cant_incompatibilidades())
 
 
-	
+def solucion_lavados(lavanderia):
+	lavados = {}
+	prendas_ordenadas = dict(sorted(lavanderia.obtener_prendas().items(), key=lambda item: -item[1]))
+
+	numero_lavado = 1
+	for prenda in prendas_ordenadas:
+		for lavado in range(1, numero_lavado + 1):
+			if lavanderia.es_compatible(prenda, lavados.setdefault(lavado, [])):
+				lavados[lavado].append(prenda)
+				break
+
+		numero_lavado += 1
+
+	print("LAVADOS: ")
+	print(lavados)
+	return lavados
+		
+
+def generar_archivo_solucion(solucion):
+	with open("solucion.txt", "w+") as sol:
+		for numero_lavado, prendas in solucion.items():
+			for prenda in prendas:
+				sol.write(prenda + " " + str(numero_lavado) + "\n")
+
+
+def main():
+	lavanderia = creacion_grafo()
+	generar_archivo_solucion(solucion_lavados(lavanderia))
+
 
 main()
--- grafo_lavanderia.py
+++ grafo_lavanderia.py
@@ -1,38 +1,22 @@
 class Lavanderia:
 
 	def __init__(self):
-		self.prendas = []
 		self.prendas_incompatibles = {}
 		self.prendas_tiempo_lavado = {}
 		self.cantidad_vertices = 0
 		self.cantidad_aristas = 0
 
-
 	def agregar_prenda(self, prenda, tiempo_lavado_prenda):
-		#if self.prendas(prenda):
-		#		return False
-
-		self.prendas_tiempo_lavado[prenda] = tiempo_lavado_prenda
+		self.prendas_tiempo_lavado[prenda] = int(tiempo_lavado_prenda)
 		self.cantidad_vertices += 1
-		return True
-
 
 	def obtener_datos_prenda(self, prenda):
 		return self.prendas_tiempo_lavado[prenda]
 
-
 	def agregar_prendas_incompatibles(self, prenda1, prenda2):
-		if not prenda1 in self.prendas_incompatibles: self.prendas_incompatibles[prenda1] = []
-		
-		#self.prendas_incompatibles.setdefault(prenda1, []).append(prenda2)
-		self.prendas_incompatibles[prenda1].append(prenda2)
+		self.prendas_incompatibles.setdefault(prenda1, []).append(prenda2)
 		self.cantidad_aristas += 1
 		return True
-
-
-	def prenda_pertenece(self, prenda):
-		return prenda in self.prendas_tiempo_lavado.keys()
-
 
 	def obtener_incompatibilidades(self, prenda):
 		return self.prendas_incompatibles[prenda]
@@ -43,4 +27,14 @@ class Lavanderia:
 	def obtener_cant_incompatibilidades(self):
 		return self.cantidad_aristas
 
+	def obtener_prendas(self):
+		return self.prendas_tiempo_lavado
 
+	def es_compatible(self, prenda1, lista_prendas):
+		compatible = True
+		
+		for prenda in lista_prendas:
+			if prenda in self.prendas_incompatibles[prenda1]:
+				compatible = False
+
+		return compatible
